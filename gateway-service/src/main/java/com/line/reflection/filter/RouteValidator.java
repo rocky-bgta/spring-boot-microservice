f53da2ae51c64455e558deb541eccf11859d23aa
Salahin <rocky.bgta@gmail.com>
2024-07-29 22:42:24 +0600

    add comment

--- gateway-service/src/main/java/com/line/reflection/filter/RouteValidator.java
+++ gateway-service/src/main/java/com/line/reflection/filter/RouteValidator.java
@@ -9,6 +9,7 @@ import java.util.function.Predicate;
 @Component
 public class RouteValidator {
 
+    // list of non secured api
     public static final List<String> openApiEndpoints = List.of(
             "/auth/register",
             "/auth/token",
